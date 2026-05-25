312d796c6d9f4f1f798d48ec00b94e4ad91662fa
Jioh Ryoo <ryoojioh510@gmail.com>
2026-05-26 02:48:45 +0900

    web prototype updated with search features and UI improvements

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import PreferencePage from "./pages/PreferencePage";
 import ResultPage from "./pages/ResultPage";
 import DetailPage from "./pages/DetailPage";
 import "./App.css";
@@ -10,7 +9,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/preferences" element={<PreferencePage />} />
         <Route path="/results" element={<ResultPage />} />
         <Route path="/restaurants/:id" element={<DetailPage />} />
       </Routes>

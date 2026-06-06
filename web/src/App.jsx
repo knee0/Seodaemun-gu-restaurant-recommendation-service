db5a3b915dcb9d6fb12b7d51857ad33e116b87af
JeongWonSeok <wjd6449@gmail.com>
2026-06-07 02:17:50 +0900

    Add GitHub Pages deployment workflow

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ResultPage from "./pages/ResultPage";
 import DetailPage from "./pages/DetailPage";
@@ -6,13 +6,13 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/results" element={<ResultPage />} />
         <Route path="/restaurants/:id" element={<DetailPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
